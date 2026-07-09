9613d485ef6e95a5a2d11ccd215053964dff5b7c
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-08 22:14:54 -0600

    Adding code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'home',
+      component: Home,
+
+    }, 
+    {
+    path: 'search',
+    component: Home
+  },
+   {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];

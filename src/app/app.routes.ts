7f9b93ec094f43ddd9fc672ba1da281a37a29579
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-08 22:28:35 -0600

    Working with ng components and components, configure @

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/components/home/home';
+import { Home } from '@pages/components/home/home';
 
 export const routes: Routes = [
     {
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./core/components/header/header";
+import { Header } from "@core/components";
 
 const NG_COMPONENTS = [RouterOutlet];
 const COMPONENTS = [Header];
